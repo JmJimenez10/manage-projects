ba09d8bc325cd5db1cbe2465c8673e3adf84eed2
José María <josemariajyh@gmail.com>
2025-02-07 21:09:38 +0100

    Project creation

---
+++ manage-projects-backend/src/main/java/dev/jmjimenez/manageprojects/ManageProjectsBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.jmjimenez.manageprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageProjectsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageProjectsBackendApplication.class, args);
+	}
+
+}
---
+++ manage-projects-backend/src/test/java/dev/jmjimenez/manageprojects/ManageProjectsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jmjimenez.manageprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageProjectsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
